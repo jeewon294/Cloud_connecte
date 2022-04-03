1e1b2bc2c9fa5babd493062717c7ef97b75b7d00
jeewon294 <dogby@naver.com>
2022-04-03 20:15:21 +0900

    2022-04-03 : hom.html(Test) controller 연동 + tymeleafe gradle에 추가

---
+++ src/main/java/com/example/test/controller/HomeController.java
@@ -0,0 +1,16 @@
+package com.example.test.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HomeController {
+    @RequestMapping(value = "/", method = RequestMethod.GET) // value = URL, method = 요청방식
+    public String home(Model model){
+        String text = "Hello World";
+        model.addAttribute("text", text);
+        return "home";
+    }
+}
