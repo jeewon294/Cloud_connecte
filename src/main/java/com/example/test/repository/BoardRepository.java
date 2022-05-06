7604b842fc1af977845b3653d311702a5b107a5d
jeewon294 <dogby@naver.com>
2022-05-06 15:30:17 +0900

    2022-05-06 : 게시물 구현(검색)

--- src/main/java/com/example/test/repository/BoardRepository.java
+++ src/main/java/com/example/test/repository/BoardRepository.java
@@ -1,10 +1,14 @@
 package com.example.test.repository;
 
 import com.example.test.entity.Board;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface BoardRepository extends JpaRepository<Board, Integer> {
+
+    Page<Board> findByTitleContaining(String searchKeyword, Pageable pageable);
 
 }
